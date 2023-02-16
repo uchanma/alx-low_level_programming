e7bf81a0a378afc6df28e5f6bc479057b29c2810
omeaku chinyere <omeakuchinyere0713@gmail.com>
2023-02-16 07:46:46 -0800

    my first C commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A c programming that prints a line of codes
+ * return: 0
+ */
+int main(void)
+{
+	puts('\"Programming is like building a multilingual puzzle");
+	return (0);
+}
