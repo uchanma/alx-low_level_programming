e4b69969c6f2c48015f273934d54c803453c1027
omeaku chinyere <omeakuchinyere0713@gmail.com>
2023-02-17 07:06:39 -0800

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+* main- entry point
+* Description: A C program that prints a line to a standard error
+* Return: returns 1
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n, standard output);
+	Return(1);
+}
+
+
