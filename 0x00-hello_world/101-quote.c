375b8e32c1013a845b7209291af4fdab68cef733
omeaku chinyere <omeakuchinyere0713@gmail.com>
2023-02-17 07:11:35 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n, standard output);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n);
 	Return(1);
 }
 
