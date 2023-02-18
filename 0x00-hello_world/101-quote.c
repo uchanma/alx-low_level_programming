40a68b609a1ce83dfbb0f26a7e17920e25f24337
omeaku chinyere <omeakuchinyere0713@gmail.com>
2023-02-18 13:01:07 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
 * main- entry point
@@ -6,8 +7,8 @@
 */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n);
-	Return(1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return(1);
 }
 
 
