8441026b0e243ee4b92dbcf587387b766edc063c
omeaku chinyere <omeakuchinyere0713@gmail.com>
2023-02-18 17:42:58 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints all single digit numbers of base 10.
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
