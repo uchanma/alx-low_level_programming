0f8ea4b29d061f12c4ec82dd3a71724e3b5a611d
omeaku chinyere <omeakuchinyere0713@gmail.com>
2023-03-11 02:44:24 -0800

    update static lib

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -4,7 +4,13 @@
 #include <ctype.h>
 #include <string.h>
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0.
+ */
 int _putchar(char c)
 {
-	    return putchar(c);
+	return (putchar(c));
 }
