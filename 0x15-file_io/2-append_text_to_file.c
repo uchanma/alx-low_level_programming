7ef47958312c00ebdc266b88c03e7dd1b2470385
omeaku chinyere <omeakuchinyere0713@gmail.com>
2023-04-23 07:53:47 -0700

    function that appends letters at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,11 +26,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (nletters = 0; text_content[nletters];)
 			nletters++;
-	}
+		rwr = write(fd, text_content, nletters);
 
-	rwr = write(fd, text_content, nletters);
-	if (rwr == -1)
-		return (-1);
+		if (rwr == -1)
+                return (-1);
+	}
 
 	close(fd);
 
